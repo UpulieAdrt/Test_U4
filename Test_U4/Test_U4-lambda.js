e5059d9b25a67495c50649ac05eb1e240f4046de
UpulieAdrt <35255575+UpulieAdrt@users.noreply.github.com>
2018-01-12 11:12:19 +0530

    as sample

--- Test_U4/Test_U4-lambda.js
+++ Test_U4/Test_U4-lambda.js
@@ -2,5 +2,7 @@ let AWS = require('aws-sdk');
 exports.handler = function(event, context, callback){
 
 console.log("success");
+console.log("Reading options from event:\n", util.inspect(event, {depth: 5}));
 
-}
+ 
+   }
